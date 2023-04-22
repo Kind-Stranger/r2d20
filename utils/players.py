02a6d8bf000c0319a5c8faa4ac7e8b5664ca2463
Kind-Stranger <103947147+Kind-Stranger@users.noreply.github.com>
2023-04-22 16:13:06 +0100

    Tidy and document

--- utils/players.py
+++ utils/players.py
@@ -1,6 +1,5 @@
 from typing import List
 
-from discord_slash import SlashContext, cog_ext
 # The below will `import discord` and override some of it's stuff
 from discord_slash.dpy_overrides import *
 from discord_slash.model import ButtonStyle
@@ -8,9 +7,14 @@ from discord_slash.utils.manage_components import (ComponentContext,
                                                    create_actionrow,
                                                    create_button,
                                                    wait_for_component)
+
 from env import misc_emojis
 
+
 class PlayerBase:
+    """Base class which allows direct access to discord.Member attributes.
+    Useful for keeping track of scores and such.
+    """
     def __init__(self, member: discord.Member):
         self._member = member
 
@@ -19,11 +23,16 @@ class PlayerBase:
         return self._member
 
     def __getattr__(self, name):
-        'Gives direct access to Member attributes'
+        """Gives direct access to discord.Member attributes"""
         if hasattr(self.member, name):
             return getattr(self.member, name)
         else:
             raise AttributeError(f"'{self.__class__.__name__} has no attribute '{name}'")
+
+def add_player_to_game_embed(embed, player, player_num):
+    embed.add_field(name=f'**P{player_num}** {player.display_name}',
+                    value='-',
+                    inline=False)
 
 async def gather_players(bot,
                          *,
@@ -37,66 +46,55 @@ async def gather_players(bot,
 
     :raises: asyncio.TimeoutError
     """
-    game_msg = game.game_msg
-    game_owner = game.game_owner
-    embed = game.embed
-    if existing_players:
-        players = list(existing_players)
-        player_ids = {p.id:0 for p in existing_players}
-        # Add existing players to embed
-        for i in range(len(players)):
-            embed.add_field(name=f'**P{i+1}** {players[i].display_name}',
-                            value='-',
-                            inline=False)
-        #
-    else:
-        players = []
+    if not existing_players:
+        existing_players = []
+    #
+    players = list(existing_players)
+    
+    for i in range(len(players)):
+        add_player_to_game_embed(game.embed, players[i], i+1)
     #
     join_button = create_button(style=ButtonStyle.blue,
-                                custom_id=f'join_{game_msg.id}',
+                                custom_id=f'join_{game.game_msg.id}',
                                 label=f'{misc_emojis["plus"]} JOIN')
     start_button = create_button(style=ButtonStyle.green,
-                                 custom_id=f'start_{game_msg.id}',
+                                 custom_id=f'start_{game.game_msg.id}',
                                  label=f'{misc_emojis["play"]} START')
-    ar = create_actionrow(join_button, start_button)
-    await game_msg.edit(embed=embed, components=[ar])
+    action_row = create_actionrow(join_button, start_button)
+    await game.game_msg.edit(embed=game.embed, components=[action_row])
 
-    def p_check(ctx: ComponentContext):
-        # Only joinable by those not already joined
-        # START only pressable by game_owner
-        return (join_button['custom_id'] == ctx.custom_id and
-                ctx.author not in players) or \
-               (start_button['custom_id'] == ctx.custom_id and
-                ctx.author == game_owner)
+    def check_join_if_not_in_game(ctx: ComponentContext):
+        """Join button can only be pressed by those not already in game"""
+        return join_button['custom_id'] == ctx.custom_id and \
+            ctx.author not in players
+
+    def check_start_if_game_owner(ctx: ComponentContext):
+        """Start button can only be pressed by game owner"""
+        return start_button['custom_id'] == ctx.custom_id and \
+            ctx.author == game.game_owner
+
+    def check_btn_press(ctx: ComponentContext):
+        return check_join_if_not_in_game(ctx) or check_start_if_game_owner(ctx)
 
     try:
-        while True:
+        while len(players) < max_players:
             btn_ctx: ComponentContext = \
-                await wait_for_component(bot, components=ar,
-                                         check=p_check,
+                await wait_for_component(bot,
+                                         components=action_row,
+                                         check=check_btn_press,
                                          timeout=timeout)
-            player = btn_ctx.author # Convenient alias
             if join_button['custom_id'] == btn_ctx.custom_id:
-                players.append(player)
-                embed.add_field(
-                    name=f'**P{len(players)}** {player.display_name}',
-                    value='-',
-                    inline=False,
-                )
-                await btn_ctx.edit_origin(embed=embed)
-
-                # Max players reached - start the game!
-                if len(players) == max_players:
-                    break
-                #
+                players.append(btn_ctx.author)
+                add_player_to_game_embed(game.embed, players[-1], len(players))
+                await btn_ctx.edit_origin(embed=game.embed)
 
             elif start_button['custom_id'] == btn_ctx.custom_id:
-                # START pressed by game owner - we can start the game
-                await btn_ctx.edit_origin(components=[])
-                break
+                break                      # Start the game
     finally:
-        # Clear up the buttons
-        await game_msg.edit(components=[])
+        try:
+            await btn_ctx.edit_origin(components=[])
+        except:
+            await game.game_msg.edit(components=[]) # Remove buttons
     #
     return players
 
