203bcda65ab385be3c83bd3710261e66780f1b88
Kind Stranger <kindstranger0@gmail.com>
2025-02-23 13:53:35 +0000

    Use log setup in discord.py; fix extension reload function

--- src/bot/r2d20.py
+++ src/bot/r2d20.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import discord
+from discord.utils import setup_logging
 from discord.ext import commands
 
 from definitions import COGS_DIR, RESOURCES_DIR, TEST_GUILDS
@@ -10,39 +11,39 @@ try:
 except ImportError:
     config = None
 
-logger = logging.getLogger()
-
+setup_logging()
 
 class R2d20(commands.Bot):
     def __init__(self, command_prefix, *, intents: discord.Intents):
         super().__init__(command_prefix, intents=intents)
+        self.logger = logging.getLogger(name=self.__class__.__name__)
         self.help_command = commands.DefaultHelpCommand()
 
     async def on_ready(self):
         """Triggered by event when bot is logged in"""
-        logger.info(f"Logged in as: {self.user}\n")
-        logger.info(
+        self.logger.info(f"Logged in as: {self.user}\n")
+        self.logger.info(
             f"Using Discord Python API version {discord.__version__}\n")
 
     async def setup_hook(self):
         """Triggered before bot is logged in"""
-        logger.debug("Executing setup hook")
+        self.logger.debug("Executing setup hook")
         if hasattr(config, 'cogs') and config.cogs:
             for cog_name in config.cogs:
                 await self.load_extension(f'cogs.{cog_name}')
-                logger.info(f"Extension {cog_name} loaded")
+                self.logger.info(f"Extension {cog_name} loaded")
         else:
             await self.load_all_cogs()
         #
         with open(os.path.join(RESOURCES_DIR, "welcome.txt")) as f:
             self.welcome_txt = f.read().strip()
         #
-        for guild_id in TEST_GUILDS:
-            logger.info("Syncing to test guild")
+        for guild in TEST_GUILDS:
+            self.logger.info("Syncing to test guild")
             try:
-                await self.sync_to_guild(guild_id)
+                await self.sync_to_guild(guild.id)
             except discord.DiscordException:
-                logger.error("Failed to sync command tree to guild")
+                self.logger.error("Failed to sync command tree to guild")
 
     async def sync_to_guild(self, guild_id: int):
         """Sync app_copmmands to a guild"""
@@ -52,7 +53,7 @@ class R2d20(commands.Bot):
 
     async def load_all_cogs(self):
         """Load every cog we can find"""
-        logger.debug("Loading all cogs")
+        self.logger.debug("Loading all cogs")
         for filename in os.listdir(COGS_DIR):
             cog_name, ext = os.path.splitext(filename)
             if cog_name.startswith('.') or cog_name.startswith('_'):
@@ -69,28 +70,30 @@ class R2d20(commands.Bot):
         Returns:
             bool: `True` if the `Cog` was loaded successfully
         """
-        action = 'reload' if cog_name in self.extensions else 'load'
+        cog_path = f"cogs.{cog_name}"
+        action = 'reload' if cog_path in self.extensions else 'load'
+        self.logger.debug(f"{action}ing {cog_path}")
         try:
             if action == 'reload':
-                await self.reload_extension(f'cogs.{cog_name}')
+                await self.reload_extension(cog_path)
             else:
-                await self.load_extension(f'cogs.{cog_name}')
+                await self.load_extension(cog_path)
         except Exception as ex:
-            logger.exception(f"Cog {action} failed: {cog_name}")
+            self.logger.exception(f"Cog {action} failed: {cog_name}")
             successful = False
         else:
-            logger.info(f"Cog {action} successful: {cog_name}")
+            self.logger.info(f"Cog {action} successful: {cog_name}")
             successful = True
         #
         return successful
 
     async def on_guild_join(self, guild: discord.Guild):
         """Triggered when bot joins a server"""
-        logger.info("Joined a guild!")
+        self.logger.info("Joined a guild!")
         try:
             await self.sync_to_guild(guild.id)
         except discord.DiscordException:
-            logger.exception("Failed to sync commands to new guild")
+            self.logger.exception("Failed to sync commands to new guild")
         if self.welcome_txt and (welcome_channel := guild.system_channel):
             try:  # Just try instead of checking permission
                 await welcome_channel.send(self.welcome_txt)
