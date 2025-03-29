4810fad96c1221f4d79e50b8dc07e6b11416970d
Kind Stranger <kindstranger0@gmail.com>
2025-03-29 21:39:58 +0000

    Set log level on all loggers

--- src/r2d20/cogs/bot_admin.py
+++ src/r2d20/cogs/bot_admin.py
@@ -15,16 +15,24 @@ logger = logging.getLogger(__name__)
 class BotAdminCog(commands.GroupCog, group_name='bot'):
     def __init__(self, bot: R2d20):
         self.bot = bot
-        self.logger = logging.getLogger(name=self.qualified_name)
 
     @app_commands.command()
     async def sync_guild(self, ctx: Interaction, guild_id: str = None):
         """Sync the command tree to the guild"""
         await ctx.response.send_message("Syncing...", ephemeral=True)
         guild = discord.Object(guild_id) if guild_id else ctx.guild
+        # self.bot.tree.clear_commands(guild=guild)
+        await self.bot.tree.sync(guild=guild)
+        await ctx.edit_original_response(content="Synced")
+
+    @app_commands.command()
+    async def clear_guild(self, ctx: Interaction, guild_id: str = None):
+        """Sync the command tree to the guild"""
+        await ctx.response.send_message("Syncing...", ephemeral=True)
+        guild = discord.Object(guild_id) if guild_id else ctx.guild
         self.bot.tree.clear_commands(guild=guild)
         await self.bot.tree.sync(guild=guild)
-        await ctx.response.edit_message(content="Synced")
+        await ctx.edit_original_response(content="Synced")
 
     @app_commands.command()
     async def sync_global(self, ctx: Interaction):
@@ -36,7 +44,11 @@ class BotAdminCog(commands.GroupCog, group_name='bot'):
     @app_commands.command()
     async def log_level(self, ctx: Interaction, level: str):
         """Set the bot's log level"""
-        self.bot.logger.setLevel(level)
+        level = level.upper()
+        for _logger in logging.root.manager.loggerDict.values():
+            if isinstance(_logger, logging.Logger):
+                _logger.setLevel(level)
+        logger.info(f"Set log level to {level}")
         await ctx.response.send_message(f"Set log level to {level}")
 
     async def cog_app_command_error(self,
@@ -49,13 +61,13 @@ class BotAdminCog(commands.GroupCog, group_name='bot'):
             error (app_commands.AppCommandError): The error that was raised
         """
         if isinstance(error, app_commands.CheckFailure):
-            self.logger.debug("Failed command check")
+            logger.debug("Failed command check")
             await ctx.response.send_message(
                 "You are not allowed to use this command",
                 ephemeral=True
             )
         else:
-            self.logger.exception(error)
+            logger.exception(error)
             if ctx.response.is_done():
                 await ctx.response.edit_message(content="Unhandled error")
             else:
